4510d7c655b49b6974e81579d1cb633b8ea69268
Isabela Müller <isabelamuller1@hotmail.com>
2026-04-22 16:31:40 -0300

    teste 4

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-[#0200F7]">
       <div className="relative flex items-center justify-center h-screen overflow-hidden">
         <video
-          className="w-[400px] h-[400px] pointer-events-none"
+          className="w-[400px] h-[400px] pointer-events-none mix-blend-screen"
+          style={{
+            filter: "brightness(0.95) contrast(1.1)",
+            backgroundColor: "transparent",
+          }}
           autoPlay
           loop
           muted
           playsInline
-          disablePictureInPicture
-          preload="auto"
         >
-          <source src="/images/arnecke-logo-moving-1.mov" type="video/mp4" />
-          {/* <source
+          <source src="/images/arnecke-logo-moving.webm" type="video/webm" />
+          <source
             src="/images/arnecke-logo-moving-1.mov"
-            type="video/quicktime"
-          /> */}
+            type='video/quicktime; codecs="hvc1"'
+          />
         </video>
       </div>
     </div>
