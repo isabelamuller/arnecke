e996b6bbe8d9ded6d829ad4901842d634e818e8d
Isabela Müller <isabelamuller1@hotmail.com>
2026-07-03 09:15:23 -0300

    remove password

--- app/coming-soon/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getMetadata } from "@/utils/getMetada";
-import { ComingSoon } from "@/views/Soon";
-import { Metadata } from "next";
-
-export const metadata: Metadata = getMetadata("Soon");
-
-export default function ComingSoonPage() {
-  return <ComingSoon />;
-}
